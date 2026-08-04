21a42fbc35a6b37c4d245cf8f2ad24fb7b7ffed2
Danish Mane <danishamne33@gmail.com>
2026-08-04 23:34:07 +0530

    Prepare Vercel deployment

--- api/index.ts
+++ api/index.ts
@@ -2,5 +2,6 @@ import serverless from "serverless-http";
 import { createServer } from "../server";
 
 const app = createServer();
+const handler = serverless(app);
 
-export default serverless(app);
+export default handler;
